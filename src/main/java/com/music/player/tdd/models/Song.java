a99d8f2e63be6833c738494494240cf4d6fc325e
M_688776 <jason.monroe@cognizant.com>
2021-03-04 17:50:16 -0600

    Integration Tests

--- src/main/java/com/music/player/tdd/models/Song.java
+++ src/main/java/com/music/player/tdd/models/Song.java
@@ -1,9 +1,16 @@
 package com.music.player.tdd.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.Duration;
 
+@Entity
 public class Song {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long Id;
 	private String title;
 	private String artist;
--- src/main/java/com/music/player/tdd/repositories/SongRepository.java
+++ src/main/java/com/music/player/tdd/repositories/SongRepository.java
@@ -2,6 +2,8 @@ package com.music.player.tdd.repositories;
 
 import com.music.player.tdd.models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SongRepository extends JpaRepository<Song, Long> {
 }
